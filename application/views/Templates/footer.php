4c519548f6214b28ead2fcdbf2a0c962fffb2c81
Matthew Walcher <mwalcher@live.ca>
2015-12-02 13:08:24 -0500

    added support JS

--- application/views/Templates/footer.php
+++ application/views/Templates/footer.php
@@ -5,12 +5,14 @@
 </div>
 <script src="<?php echo base_url().'js/vendor/jquery.js';?>"></script>
 <script src="<?php echo base_url().'js/foundation.min.js';?>"></script>
+<script src="<?php echo base_url().'js/foundation/foundation.offcanvas.js';?>"></script>
 <script>
 $(document).foundation();
 </script>
 <script type="text/javascript">
 	baseURL = "<?php echo base_url(); ?>";
 </script>
+<script src="<?php echo base_url().'js/retina.min.js';?>">></script>
 <script type="text/javascript" src="<?php echo base_url().'js/clock.js';?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'js/main.js';?>"></script>
 </body>
