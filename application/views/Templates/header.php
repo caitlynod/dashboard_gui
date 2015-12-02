7bffe48c42a8adc1970e89063aaeef92f5020847
caitlynod <caitlynodriscoll@gmail.com>
2015-12-02 13:53:12 -0500

    off canvas nav

--- application/views/Templates/header.php
+++ application/views/Templates/header.php
@@ -6,6 +6,7 @@
     <title><?php echo $title;?></title>
     <link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?php echo base_url('css/foundation.css');?>" />
+    <link rel="stylesheet" href="<?php echo base_url('fonts/stylesheet.css');?>" />
     <link rel="stylesheet" href="<?php echo base_url('css/main.css');?>" />
     <script src="<?php echo base_url('js/vendor/modernizr.js');?>"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXjC-Us_OJ7c_xUiYjNQZ0RKnuZOjz4S0"></script>
--- application/views/Templates/navigation.php
+++ application/views/Templates/navigation.php
@@ -2,26 +2,37 @@
 <div class="off-canvas-wrap" data-offcanvas>
   <div class="inner-wrap">
     <nav class="tab-bar">
-      <section class="small-4 columns">
-        <div class="left">
-		<h2 id="mainClock" class="left"></h2>
-		<div class="right date">
-			<p ><?php echo date('l');?></p>
-		<p ><?php echo date('F d Y');?></p>
-		</div>
+      <a class="left-off-canvas-toggle menu-icon left" href="#"><span></span></a>
+      
 		
-	</div>
+        
+      </section>
+    </nav>
+
+    <aside class="left-off-canvas-menu">
+      <section class="small-4 columns dateTime">
+        <div class="left">
+    <h2 id="mainClock" class="left"></h2>
+    <div class="right date">
+      <p ><?php echo date('l');?></p>
+    <p ><?php echo date('F d Y');?></p>
+    </div>
+    
+  </div>
       </section>
 
       <section class="medium-4 columns">
-        <h1 class="title text-center">Volt</h1>
+       <img class="logo right" src="<?php echo base_url().'images/logo.png';?>">
       </section>
+
+
       <section class="medium-4 columns right">
-        <div id="changeUser" class="small-8 columns">
-          <a href="" class="left">change user</a>
+        <div id="changeUser" class="small-6 columns">
+          <a href="" class="left change">change user</a>
           <img class="userChange" src="<?php echo base_url().'images/change_icon.png';?>">
         </div>
-		
-        <a class="right-off-canvas-toggle menu-icon right" href="#"><span></span></a>
-      </section>
-    </nav>
+</section>
+        <section class="medium-12 columns right">
+          <h2>Please select a user</h2>
+        </section>
+    </aside>
