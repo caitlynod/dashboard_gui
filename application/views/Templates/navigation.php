5e83df737792e796521ded79fef92de16dffe1d1
caitlynod <caitlynodriscoll@gmail.com>
2015-12-02 12:58:06 -0500

    Updated

--- application/views/Templates/navigation.php
+++ application/views/Templates/navigation.php
@@ -5,19 +5,23 @@
       <section class="small-4 columns">
         <div class="left">
 		<h2 id="mainClock" class="left"></h2>
-		<div class="right">
-			<p class="left"><?php echo date('l');?></p><br>
-		<p class="left"><?php echo date('F d Y');?></p>
+		<div class="right date">
+			<p ><?php echo date('l');?></p>
+		<p ><?php echo date('F d Y');?></p>
 		</div>
 		
 	</div>
       </section>
 
       <section class="medium-4 columns">
-        <h1 class="title">Volt</h1>
+        <h1 class="title text-center">Volt</h1>
       </section>
       <section class="medium-4 columns right">
-		<a href="">change user</a>
-        <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+        <div id="changeUser" class="small-8 columns">
+          <a href="" class="left">change user</a>
+          <img class="userChange" src="<?php echo base_url().'images/change_icon.png';?>">
+        </div>
+		
+        <a class="right-off-canvas-toggle menu-icon right" href="#"><span></span></a>
       </section>
     </nav>
