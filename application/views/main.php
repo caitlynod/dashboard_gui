ffafb3d084c7e0720f698b2849945c3e13e7ceb9
Matthew Walcher <mwalcher@live.ca>
2015-12-02 18:46:18 -0500

    absolute position on nav bar + set up main dash

--- application/views/main.php
+++ application/views/main.php
@@ -1,8 +1,8 @@
 <div id="topClock" class="text-center"></div>
 <div id="navBorder"></div>
-<ul class="small-block-grid-4">
+<ul id="mainDash" class="small-block-grid-4">
 	<li><img src="<?php echo base_url().'images/weather_icon.png';?>"></li>
 	<li><img src="<?php echo base_url().'images/clock_icon.png';?>"></li>
-	<li><img src="<?php echo base_url().'images/gps_icon.png';?>"></li>
+	<li><img src="<?php echo base_url().'images/trip_icon.png';?>"></li>
 	<li><img src="<?php echo base_url().'images/radio_icon.png';?>"></li>
 </ul>
