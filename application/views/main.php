36de75c392fdaf212b9d205521c10d195d7f7fc2
Matthew Walcher <mwalcher@live.ca>
2015-12-02 19:10:53 -0500

    updated lists

--- application/views/main.php
+++ application/views/main.php
@@ -3,12 +3,24 @@
 <ul id="mainDash" class="small-block-grid-4 text-center">
 	<li>
 		<img src="<?php echo base_url().'images/weather_icon.png';?>">
-		<h3 class="selectUser">weather</h3>
+		<h3 class="userhead">weather</h3>
+		<p class="white">London ON</p>
+		<div class="dashInfo">	
+		</div>
 	</li>
 	<li>
 		<img src="<?php echo base_url().'images/clock_icon.png';?>">
-		<h3 class="selectUser">clock</h3>
+		<h3 class="userhead">clock</h3>
+		<p class="white">London ON</p>
 	</li>
-	<li><img src="<?php echo base_url().'images/trip_icon.png';?>"></li>
-	<li><img src="<?php echo base_url().'images/radio_icon.png';?>"></li>
+	<li>
+		<img src="<?php echo base_url().'images/trip_icon.png';?>">
+		<h3 class="userhead">trip</h3>
+		<p class="white">Woodstock ON</p>
+	</li>
+	<li>
+		<img src="<?php echo base_url().'images/radio_icon.png';?>">
+		<h3 class="userhead">radio</h3>
+		<p class="white">FM Radio</p>
+	</li>
 </ul>
