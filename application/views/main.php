7af2f553e7f532862b90d7c89a53f41db064ea3a
Matthew Walcher <mwalcher@live.ca>
2015-12-02 19:02:50 -0500

    adding titles

--- application/views/main.php
+++ application/views/main.php
@@ -1,8 +1,14 @@
 <div id="topClock" class="text-center"></div>
 <div id="navBorder"></div>
-<ul id="mainDash" class="small-block-grid-4">
-	<li><img src="<?php echo base_url().'images/weather_icon.png';?>"></li>
-	<li><img src="<?php echo base_url().'images/clock_icon.png';?>"></li>
+<ul id="mainDash" class="small-block-grid-4 text-center">
+	<li>
+		<img src="<?php echo base_url().'images/weather_icon.png';?>">
+		<h3 class="selectUser">weather</h3>
+	</li>
+	<li>
+		<img src="<?php echo base_url().'images/clock_icon.png';?>">
+		<h3 class="selectUser">clock</h3>
+	</li>
 	<li><img src="<?php echo base_url().'images/trip_icon.png';?>"></li>
 	<li><img src="<?php echo base_url().'images/radio_icon.png';?>"></li>
 </ul>
