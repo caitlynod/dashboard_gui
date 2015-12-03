37874da65304c4fb1e7231426e0191c6a65f208b
caitlynod <caitlynodriscoll@gmail.com>
2015-12-02 19:10:42 -0500

    nav fixes

--- application/views/Templates/navigation.php
+++ application/views/Templates/navigation.php
@@ -35,15 +35,13 @@
             <p>Hello! Who will be driving today?</p>
           </div>
           <hr>
-          <div class="small-block-grid-4 columns users">
-            <ul class="small-block-grid-4 columns users">
-              
+          <div class="users">
+            <ul class="small-block-grid-4 columns userProf">
+              <li><img class="profPic" src="<?php echo base_url().'images/user_icon.png';?>"><h2 class="profile">John</h2></li>
+              <li><img class="profPic" src="<?php echo base_url().'images/user_icon.png';?>"><h2 class="profile">Annie</h2></li>
+               <li><img class="profPic" src="<?php echo base_url().'images/user_icon.png';?>"><h2 class="profile">Guest</h2></li>
+               <li><img class="profPic" src="<?php echo base_url().'images/adduser_icon.png';?>"><h2 class="profile">Add User</h2></li>
             </ul>
-
-            <div><p>Cait</p></div>
-            <div><p>Cait</p></div>
-            <div><p>Cait</p></div>
-            <div><p>Cait</p></div>
           </div>
           
         </section>
