dfcc5a4fe22df9f1acd58fb8767f3d58c4ab65a0
caitlynod <caitlynodriscoll@gmail.com>
2015-12-05 14:03:48 -0500

    Linking contact to search

--- application/views/phoneCenter/contacts.php
+++ application/views/phoneCenter/contacts.php
@@ -7,7 +7,7 @@
     <input id="resultSlider" type="range" min="1" value="1" max="4">
     <ul id="contactList" class="small-block-grid-6 columns contacts">
         <li><img class="navPic" src="<?php echo base_url().'images/adduser_icon.png';?>"><h2 class="contactName">add user</h2></li>
-        <li><img class="navPic" src="<?php echo base_url().'images/adduser_icon.png';?>"><h2 class="contactName">search</h2></li>
+        <li><a href="<?php echo base_url()."index.php/phone/contactSearch";?>"><img class="navPic" src="<?php echo base_url().'images/adduser_icon.png';?>"><h2 class="contactName">search</h2></a></li>
         <?php foreach($contacts as $contact):?>
     		<li><img class="navPic" src="<?php echo base_url().'images/'.$contact['contacts_img'];?>"><h2 class="contactName"><?php echo $contact['contacts_fname'];?></h2><p class="contactName"><?php echo $contact['contacts_lname'];?></p></li>
         <?php endforeach;?>
