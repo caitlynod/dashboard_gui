d31cedfbc3814cc7227ff39e5f657293d59a37cd
Matthew Walcher <mwalcher@live.ca>
2015-12-06 12:02:33 -0500

    thumb images

--- application/views/statusCenter/status.php
+++ application/views/statusCenter/status.php
@@ -3,8 +3,32 @@
 <a href="<?php echo base_url()."index.php/dashboard/volt";?>"><img class="close" src="<?php echo base_url()."images/close_icon.png";?>"></a>
 <h2 id="addressbook" class="text-center userhead">volt status</h2>
 <div class="statusImages">
-        
+    <div class="row">
+    <div class="small-8 columns">
+        <img src="<?php echo base_url().'images/carstatus_icon.png';?>">
+    </div>
+    <div class="small-4 columns">
+        <img src="<?php echo base_url().'images/cargraph_icon.png';?>">
+    </div>
+    </div>
 </div>
+<div class="small-8 small-centered columns">
 <ul class="small-block-grid-4 text-center">
-        
+    <li>
+        <img src="<?php echo base_url().'images/power_icon.png';?>">
+        <p>powerflow</p>
+    </li>
+    <li>
+        <img src="<?php echo base_url().'images/charge_icon.png';?>">
+        <p>charging</p>
+    </li>
+    <li>
+        <img src="<?php echo base_url().'images/carinfo_icon.png';?>">
+        <p>info</p>
+    </li>
+    <li>
+        <img src="<?php echo base_url().'images/diagnostics_icon.png';?>">
+        <p>diagnostics</p>
+    </li>
 </ul>
+</div>
