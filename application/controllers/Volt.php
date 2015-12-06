b7f238be7abf67003563f88b790d7e96f552c598
Matthew Walcher <mwalcher@live.ca>
2015-12-06 11:42:57 -0500

    volt icons

--- application/controllers/Volt.php
+++ application/controllers/Volt.php
@@ -8,11 +8,12 @@ class Volt extends CI_Controller {
 	}
 
 	public function index(){
-		$data['title'] = 'Volt';
+		$data['title'] = 'Volt Status';
 		$data['name'] = $this->session->userdata('name');
 		$this->load->view('Templates/header', $data);
 		$this->load->view('navigation/navigationClosed');
 		$this->load->view('navigation/mainNav');
+		$this->load->view('statusCenter/status');
 		$this->load->view('Templates/footer');
 		$this->load->view('Templates/dashFooter');
 
---
+++ application/views/statusCenter/status.php
@@ -0,0 +1,10 @@
+<div id="topClock" class="text-center"><span id="topClockTime"></span> | <?php echo $title;?></div>
+<div class="marginTop"></div>
+<a href="<?php echo base_url()."index.php/dashboard/volt";?>"><img class="close" src="<?php echo base_url()."images/close_icon.png";?>"></a>
+<h2 id="addressbook" class="text-center userhead">volt status</h2>
+<div class="statusImages">
+        
+</div>
+<ul class="small-block-grid-4 text-center">
+        
+</ul>
